526ebdd5e0657acf989c8b230d56bbca1746bd3f
Marcos Camacena <camacenamarcos24@gmail.com>
2024-02-29 10:42:37 -0800

    frist page of front end

---
+++ client/src/pages/WorkoutDetail.jsx
@@ -0,0 +1,8 @@
+function WorkoutDetail(){
+    return(
+        <div>
+            this is the workout Detail page 
+        </div>
+    )
+}
+export default WorkoutDetail
---
+++ client/src/pages/WorkoutList.jsx
@@ -0,0 +1,8 @@
+function WorkoutList(){
+    return(
+        <div>
+            this is the workout list  page 
+        </div>
+    )
+}
+export default WorkoutList
