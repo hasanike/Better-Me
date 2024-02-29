fc71a2e7f9997f4c9828b9d4783f955a2b343ab6
Marcos Camacena <camacenamarcos24@gmail.com>
2024-02-29 12:00:00 -0800

    added files

--- client/src/pages/WorkoutList.jsx
+++ client/src/pages/WorkoutList.jsx
@@ -1,8 +1,29 @@
-function WorkoutList(){
-    return(
-        <div>
-            this is the workout list  page 
-        </div>
-    )
+import React from 'react';
+
+function WorkoutList() {
+    return (
+        <nav>
+            <h1>This is the workout list page</h1>
+            <ul>
+                <li><button onClick={handleLogin}>Login</button></li>
+                <li><button onClick={handleLogout}>Logout</button></li>
+                <li><button onClick={handleProfile}>Profile</button></li>
+            </ul>
+        </nav>
+    );
 }
-export default WorkoutList
+
+
+const handleLogin = () => {
+    
+};
+
+const handleLogout = () => {
+    
+};
+
+const handleProfile = () => {
+   
+};
+
+export default WorkoutList;
