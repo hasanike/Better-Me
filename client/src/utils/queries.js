de6b50720f463ea674827c3238d720c511577140
hkeidenb <hasani.eidenberger@gmail.com>
2024-03-05 14:32:36 -0800

    reworked backend

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,4 +1,20 @@
 import { gql } from "@apollo/client"
 
 export const QUERY_WORKOUTS = gql`
+query Query {
+    workouts {
+      _id
+      createdAt
+      workoutName
+      workoutDescription
+      exercises {
+        _id
+        name
+        createdAt
+        reps
+        sets
+        weight
+      }
+    }
+  }
 `
