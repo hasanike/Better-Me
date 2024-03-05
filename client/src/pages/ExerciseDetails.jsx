78ecdfafca47673a02771c4f076bf44a2cecf06b
hkeidenb <hasani.eidenberger@gmail.com>
2024-03-05 13:27:35 -0800

    edit react

--- client/src/pages/ExerciseDetails.jsx
+++ client/src/pages/ExerciseDetails.jsx
@@ -13,34 +13,6 @@ import {
 
 const FixedMenuLayout = () => (
   <div>
-    <Menu fixed='top' inverted>
-      <Container>
-        <Menu.Item as='a' header>
-          <Image size='mini' src='/logo.png' style={{ marginRight: '1.5em' }} />
-          Better Me
-        </Menu.Item>
-        <Menu.Item as='a'>Home</Menu.Item>
-
-        <Dropdown item simple text='Dropdown'>
-          <Dropdown.Menu>
-            <Dropdown.Item>List Item</Dropdown.Item>
-            <Dropdown.Item>List Item</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Header>Header Item</Dropdown.Header>
-            <Dropdown.Item>
-              <i className='dropdown icon' />
-              <span className='text'>Submenu</span>
-              <Dropdown.Menu>
-                <Dropdown.Item>List Item</Dropdown.Item>
-                <Dropdown.Item>List Item</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown.Item>
-            <Dropdown.Item>List Item</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </Container>
-    </Menu>
-
     <Container text style={{ marginTop: '7em' }}>
       <Header as='h1'>Plank</Header>
       <p>Planking is a full-body excercise. </p>
--- client/src/pages/WorkoutList.jsx
+++ client/src/pages/WorkoutList.jsx
@@ -1,29 +1,68 @@
-import React from 'react';
+import React from 'react'
+import { Container, Divider, Grid, Header, Icon } from 'semantic-ui-react'
 
-function WorkoutList() {
-    return (
-        <nav>
-            <h1>This is the workout list page</h1>
-            <ul>
-                <li><button onClick={handleLogin}>Login</button></li>
-                <li><button onClick={handleLogout}>Logout</button></li>
-                <li><button onClick={handleProfile}>Profile</button></li>
-            </ul>
-        </nav>
-    );
-}
+// Ported from the amazing codepen by Alexei Popkov.
+// https://codepen.io/Reystleen/pen/oydqxz
+
+const AnotherGridLayout = () => (
+  <Container>
+    {/* Heads up! We apply there some custom styling, you usually will not need it. */}
+    <style>
+      {`
+      html, body {
+        background-color: black
+        !important;
+      }
+
+      p {
+        align-content: center;
+        background-color: gray;
+        color: #fff;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        min-height: 6em;
+        text-align: center;
+      }
+
+      p > span {
+        opacity: 0.4;
+        text-align: center;
+      }
+    }
+    `}
+    </style>
 
 
-const handleLogin = () => {
-    
-};
+    <Header as='h2' inverted textAlign='center'>
+      Excercise List
+    </Header>
+    <Grid celled='internally' columns={3}>
+      <Grid.Row>
+        <Grid.Column>
+          <p>Plank </p>
+        </Grid.Column>
+        <Grid.Column>
+          <p> Squat </p>
+        </Grid.Column>
+        <Grid.Column>
+          <p> Lunge </p>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column>
+          <p>Ab Crunch</p>
+        </Grid.Column>
+        <Grid.Column>
+          <p>Pigeon</p>
+        </Grid.Column>
+        <Grid.Column>
+          <p> Push-up </p>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
 
-const handleLogout = () => {
-    
-};
+      </Container>
+)
 
-const handleProfile = () => {
-   
-};
-
-export default WorkoutList;
+export default AnotherGridLayout
